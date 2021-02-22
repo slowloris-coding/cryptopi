9a664da317a916c3188ee6340a65cf848a459567
Elio Teuber <elioteuber@outlook.com>
2021-02-22 23:32:44 +0100

    some lcd testing

--- artifacts/test_lcd.py
+++ artifacts/test_lcd.py
@@ -2,7 +2,7 @@ import I2C_LCD_driver
 from time import *
 
 mylcd = I2C_LCD_driver.lcd()
-mylcd.lcd_display_string("***----------------***", 1, 0)
+mylcd.lcd_display_string("***--------------***", 1, 0)
 mylcd.lcd_display_string("Hello", 2, 4)
 mylcd.lcd_display_string("World!", 3, 4)
-mylcd.lcd_display_string("***----------------***", 1, 0)
+mylcd.lcd_display_string("***--------------***", 1, 0)
